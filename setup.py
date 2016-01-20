be1f72a2c8459363058e1957b59cb48fbe6a2911
yagmur <yagmur@saltmaster>
2016-01-20 22:48:49 +0200

    setup.py download_url typo

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ setup(
     maintainer='Yagmur Guraslan',
     maintainer_email='guraslan@gmail.com',
     url='https://github.com/guraslan/stayontop',
-    download_url = 'https://github.com/guraslan/stayontop/tarball/0.1'
+    download_url = 'https://github.com/guraslan/stayontop/tarball/0.1a0'
     license='BSD',
     packages=['stayontop'],
     entry_points={
